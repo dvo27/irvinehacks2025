aacf85115a1caed45a4f26852744c74dd59c5d91
sabrinaprc <sabrinyp@uci.edu>
2025-01-25 14:23:46 -0800

    Added new pre trained fridge detection model along with a sample testing image

--- geminiAPI.py
+++ geminiAPI.py
@@ -1,6 +1,15 @@
-import google.generativeai as genai
+# import google.generativeai as genai
+from inference_sdk import InferenceHTTPClient
 
-genai.configure(api_key="AIzaSyAxpK_2MdQlPoAjfFAXwAEcRsOia46o5SM")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
+CLIENT = InferenceHTTPClient(
+    api_url="X2ztjBd1fkNwfVwJKQKv",
+    api_key="API_KEY"
+)
+
+result = CLIENT.infer(samplefridgeimage.jpeg, model_id="aicook-lcv4d/3")
+print(result)
+
+# genai.configure(api_key="AIzaSyAxpK_2MdQlPoAjfFAXwAEcRsOia46o5SM")
+# model = genai.GenerativeModel("gemini-1.5-flash")
+# response = model.generate_content("Explain how AI works")
+# print(response.text)
