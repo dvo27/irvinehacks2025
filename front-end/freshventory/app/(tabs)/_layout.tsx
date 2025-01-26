a694ac2852639465b4cfc135a79f64fe4e05b365
dvo27 <danielnvo08@gmail.com>
2025-01-25 19:00:57 -0800

    Implemented Onboarding Draft

--- front-end/freshventory/app/(tabs)/_layout.tsx
+++ front-end/freshventory/app/(tabs)/_layout.tsx
@@ -1,40 +1,62 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { Ionicons } from '@expo/vector-icons';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
+import React, { useEffect, useState } from "react"
+import FontAwesome from "@expo/vector-icons/FontAwesome"
+import { Ionicons } from "@expo/vector-icons"
+import { MaterialCommunityIcons } from "@expo/vector-icons"
+import { Tabs, Redirect } from "expo-router"
+import * as SecureStore from "expo-secure-store"
 
 export default function TabLayout() {
+  const [isOnboardingCompleted, setIsOnboardingCompleted] = useState<boolean | null>(null)
+
+  useEffect(() => {
+    checkOnboardingStatus()
+  }, [])
+
+  const checkOnboardingStatus = async () => {
+    const status = await SecureStore.getItemAsync("onboardingCompleted")
+    setIsOnboardingCompleted(status === "true")
+  }
+
+  if (isOnboardingCompleted === null) {
+    // Still loading, you might want to show a loading screen here
+    return null
+  }
+
+  if (isOnboardingCompleted === false) {
+    return <Redirect href="/onboarding" />
+  }
+
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
       <Tabs.Screen
         name="recipes"
         options={{
-          title: 'Recipes',
+          title: "Recipes",
           tabBarIcon: ({ color }) => <MaterialCommunityIcons size={28} name="chef-hat" color={color} />,
         }}
       />
-      {/* default screen is scan which has to be named index */}
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Scan',
+          title: "Scan",
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="camera" color={color} />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
-          title: 'Profile',
+          title: "Profile",
           tabBarIcon: ({ color }) => <Ionicons size={28} name="person" color={color} />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
-          title: 'Settings',
+          title: "Settings",
           tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
         }}
       />
     </Tabs>
-  );
+  )
 }
+
